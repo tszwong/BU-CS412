c128eb117eda2035e0a03ca2cc4258b9615efbae
tszwong <wongt@bu.edu>
2024-09-30 15:50:29 -0400

    fixed file structure

--- cs412/urls.py
+++ cs412/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hw/", include("hw.urls")),
     path("quotes/", include("quotes.urls")),
     path("formdata/", include("formdata.urls")),
-    path("restaurant/", include("restaurant.urls")),
+    path("restaurant/", lambda request: redirect("/", permanent=True)),
     path("", include("restaurants.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
