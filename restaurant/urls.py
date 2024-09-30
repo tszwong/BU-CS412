c280545856a87e9ee2cc7cd0760d5c9cf36a72ea
tszwong <wongt@bu.edu>
2024-09-30 16:49:36 -0400

    fix

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,7 +7,7 @@ from . import views
 # create a list of URLs for this app
 urlpatterns = [
     # path(url, view, name)
-    path("main/", views.main, name="main"),
-    path("order/", views.order, name="order"),
-    path("confirmation/", views.confirmation, name="confirmation"),
+    path("", views.main, name="main"),
+    path("order", views.order, name="order"),
+    path("confirmation", views.confirmation, name="confirmation"),
 ]
